813297f7ab4615d47d4b548e169ad075b6ac7c32
aryan21710 <aryan21710@gmail.com>
2020-06-06 18:01:41 +0530

    WEBPACK PRODUCTION BUILD WHICH MAKES DIFFERENT CSS, BUNDLE.JS AND BUNDLE.JS.MAP FILE

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import Approutes  from "./routers/Approutes";
 import 'normalize.css/normalize.css';
 import "../styles/styles.css";
-import "../styles/react-table.css"
+// import "../styles/react-table.css"
 import { Provider } from "react-redux";
 import store from './redux/store/store'
 
