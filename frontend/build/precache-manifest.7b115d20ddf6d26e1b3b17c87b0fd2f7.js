6199449303a66618a062565c010fa7a0f8e85f53
aryan21710 <aryan21710@gmail.com>
2020-06-08 19:06:06 +0530

    GENERATED THE BUILD

---
+++ frontend/build/precache-manifest.7b115d20ddf6d26e1b3b17c87b0fd2f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76279cf0ebc88c65a9eeeabaf726e5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61a79f6a090575985ef8",
+    "url": "/static/css/2.816498fe.chunk.css"
+  },
+  {
+    "revision": "6d0d3f525bed86234e7d",
+    "url": "/static/css/main.a6272d90.chunk.css"
+  },
+  {
+    "revision": "61a79f6a090575985ef8",
+    "url": "/static/js/2.cc189b0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cc189b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0d3f525bed86234e7d",
+    "url": "/static/js/main.0a2debbc.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "a3e44141e9bf6dfd35d483fbb2fd86f3",
+    "url": "/static/media/office.a3e44141.png"
+  }
+]);
