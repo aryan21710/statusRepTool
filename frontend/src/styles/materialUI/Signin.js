69795ee9635f95f9580cb554adb5de80e36995ea
aryan21710 <aryan21710@gmail.com>
2021-02-12 01:39:46 +0530

    LOGIN SCREEN UI IS READY USING MATERIAL UI

--- frontend/src/styles/materialUI/Signin.js
+++ frontend/src/styles/materialUI/Signin.js
@@ -1,11 +1,44 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles({
-    mainContainer: {
-      position: 'relative',
-      top: 0,
-      left: 0,
-      padding: 0,
-      margin: 0
-    },
-  });
+	mainContainer: {
+		position: 'relative',
+		top: 0,
+		left: 0,
+		padding: 0,
+		margin: 0,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	signInForm: {
+		display: 'flex',
+		justifyContent: 'flex-start',
+		flexDirection: 'column',
+		alignItems: 'center',
+		width: '27vw',
+		background: 'rgba(0,0,0,0.8)',
+		borderRadius: '20px',
+		padding: '4vh 0vw',
+	},
+	inputFields: {
+		width: '20vw',
+		margin: '1vh 0vw',
+		color: 'white',
+		fontSize: '20px',
+		borderBottom: '1px solid blue',
+	},
+	title: {
+		color: 'white',
+		marginTop: '5px',
+	},
+	heading: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		width: '55%',
+	},
+	icon: {
+		color: 'white',
+	},
+});
--- frontend/src/views/SignIn.js
+++ frontend/src/views/SignIn.js
@@ -1,178 +1,64 @@
-import React, { useEffect, useState, useRef } from "react";
-import LoginHeader from "../components/LoginHeader";
-import { useHistory } from "react-router-dom";
-import { shallowEqual, useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { signUpAction,signInAction } from "../redux/action/LoginAction";
-import { Box } from '@material-ui/core';
-import {useStyles} from '../styles/materialUI/Signin'
-import { signout,saveJwtToLocalStorage } from "../api/userAuth";
+import React, { useEffect, useState, useRef } from 'react';
+import LoginHeader from '../components/LoginHeader';
+import { useHistory } from 'react-router-dom';
+import { shallowEqual, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { signUpAction, signInAction } from '../redux/action/LoginAction';
+import { Box, FormControl, TextField, Typography, Button } from '@material-ui/core';
+import { useStyles } from '../styles/materialUI/Signin';
+import { signout, saveJwtToLocalStorage } from '../api/userAuth';
+import BallotIcon from '@material-ui/icons/Ballot';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { green, red } from '@material-ui/core/colors';
+import { GoogleLogin } from 'react-google-login';
 
-const SignIn=(props)=>{
-    const classes = useStyles(props);
+const SignIn = props => {
+	const classes = useStyles(props);
+	return (
+		<Box width="100vw" height="100vh" className={classes.mainContainer}>
+			<FormControl className={classes.signInForm}>
+				<Box className={classes.heading}>
+					<BallotIcon fontSize="large" className={classes.icon} />
+					<Typography variant="h4" gutterBottom={true} className={classes.title}>
+						Status Reporting Tool
+					</Typography>
+				</Box>
 
-  const history = useHistory();
-  const dispatch = useDispatch();
+				<TextField
+					variant="standard"
+					label="Email:-"
+					className={classes.inputFields}
+					type="email"
+					InputLabelProps={{
+						style: { color: '#fff' },
+					}}
+				/>
+				<TextField
+					label="Password:-"
+					className={classes.inputFields}
+					variant="standard"
+					type="password"
+					InputLabelProps={{
+						style: { color: '#fff' },
+					}}
+				/>
+				<Button
+					style={{ margin: '3vh 0vw', fontSize: '1vw', padding: '1vh 5vw' }}
+					size="large"
+					variant="contained"
+					color="secondary"
+				>
+					Login
+				</Button>
 
-  const emailInput = useRef();
-  const passwdInput = useRef();
-  const submitInput = useRef();
+				<GoogleLogin
+					clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+					buttonText="Login"
+					cookiePolicy={'single_host_origin'}
+				/>
+			</FormControl>
+		</Box>
+	);
+};
 
-  useEffect(() => {
-    emailInput.current.focus();
-    signout()
-  }, []);
-
-  const [values, setValues] = useState({
-    password: "",
-    email: "",
-    serverError: "",
-    success: false,
-    signUpSuccess: false,
-  });
-
-  const { password, email, serverError, success, signUpSuccess } = values;
-
-  const signUpSuccessFromRedux = useSelector(
-    (state) => state.signUpReducer.success,
-    shallowEqual
-  );
-
-
-
-  console.log("signUpSuccessFromRedux", signUpSuccessFromRedux);
-  useEffect(() => {
-    if (success) {
-      history.push("/view");
-    }
-    // eslint-disable-next-line
-  }, [success]);
-
-  useEffect(() => {
-    console.log('useeffect is called',signUpSuccess)
-    if (signUpSuccessFromRedux) {
-      setTimeout(() => {
-        dispatch(signUpAction({ email, success: false }));
-      }, 2000);
-    }
-    setValues({
-      ...values,
-      signUpSuccess: signUpSuccessFromRedux,
-    });
-        // eslint-disable-next-line
-  }, [signUpSuccess]);
-
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    // console.log('before dispatch',values)
-    const {email,password}=values
-    console.log('email,password',email,password)
-    const data=await dispatch(signInAction({ email, password }))
-    console.log('data back from axios post  for signInAction',data)
-
-    if (data.error) {
-        setValues({ ...values, serverError: data.error, success: false });
-      } else {
-        const {email,name,_id } = data.userCred
-        saveJwtToLocalStorage(data.token, { name, email,_id }, () => {
-          setValues({
-            email,
-            serverError: false,
-            success: true,
-          });
-        });
-      }
-    
-  };
-
-
-  /**
-   * This will update the state variable email and password with the user entered email and password.
-   * @param  {object} e Event Object
-   * @returns {object} :- Updates the state variable called values with email & password leaving serverError and signUpSuccess to false;
-   */
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    // console.log('name and value',name,':',value)
-    setValues({
-      ...values,
-      [name]: value,
-      serverError: false,
-      signUpSuccess: false,
-    });
-  };
-
-
-
-  /**
-   *  This method will be invoked when user uses keyboard keydown button instead of mouse cursor to navigate
-   *  between email and password input boxes.
-   * @param  {object} e Event Object
-   */
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      if (e.target.name === "email") {
-        passwdInput.current.focus();
-      } else if (e.target.name === "password") {
-        submitInput.current.focus();
-      }
-    }
-  };
-
-    return (
-      <Box width="100vw" height="100vh" className={classes.mainContainer}>
-        <form onSubmit={handleSubmit} className="loginForm">
-        <LoginHeader/>
-
-          {email.length > 0 && <div className="secondaryBkgLogin"></div>}
-          <p>Login</p>
-          <hr />
-
-          <input
-            className="email"
-            value={email}
-            name="email"
-            onKeyDown={handleKeyDown}
-            onChange={handleChange}
-            type="email"
-            ref={emailInput}
-            placeholder="EMAIL ADDRESS"
-          />
-          <input
-            className="passwd"
-            value={password}
-            type="password"
-            name="password"
-            onKeyDown={handleKeyDown}
-            onChange={handleChange}
-            ref={passwdInput}
-            placeholder="PASSWORD"
-
-          />
-          <button
-          className="loginBtn"
-          ref={submitInput}
-          onKeyDown={handleKeyDown}
-          onClick={handleSubmit}
-        >
-          Sign In
-        </button>
-        {serverError && (
-          <div className="signUperror flexStyling">{serverError}</div>
-        )}
-        {serverError
-          ? undefined
-          : signUpSuccess && (
-              <div className="signUpsuccess flexStyling">
-                SIGNUP SUCCESS. PLEASE SIGNIN NOW
-              </div>
-            )}
-        </form>
-        
-      </Box>
-    );
-  
-}
-
-export default SignIn
+export default SignIn;
